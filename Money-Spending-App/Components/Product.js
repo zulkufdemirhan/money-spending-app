e1a1b50fe762d1856cb63b6fcc835136b0407e1b
Zulkuf Demırhan <z.demirhan95@gmail.com>
2022-04-26 15:52:20 +0300

    Layout Fixed

--- Money-Spending-App/Components/Product.js
+++ Money-Spending-App/Components/Product.js
@@ -1,17 +1,13 @@
 import React from 'react'
 
 export default function Product({product,shopping,setShopping,total,money}) {
-
     const shoppingItem = shopping.find(item=>item.id === product.id)
-
     const addShopping=()=>{
         const checkShopping = shopping.find(item=>item.id === product.id)
-
         if(checkShopping){
             checkShopping.amount += 1
             setShopping([...shopping.filter(item=>item.id !== product.id),checkShopping])
         }
-        
         else{
             setShopping([...shopping,{
                 id: product.id,
@@ -19,41 +15,26 @@ export default function Product({product,shopping,setShopping,total,money}) {
             }])
         }
     }
-
     const removeShopping= () =>{
-
         const checkShopping = shopping.find(item=>item.id === product.id)
         const allShopping = shopping.filter(item=>item.id !== product.id)
         checkShopping.amount -= 1
         if(checkShopping.amount === 0){
             setShopping([...allShopping])
-        }
-        else{
+        }else{
             setShopping([...allShopping,checkShopping])
-        }
-    }
-        
-
+        }}        
 return (
     <div className='product-box'>
         <div className='product'>
-
             <h3>{product.title}</h3>
-
                 <div className='price'>${product.price}</div>
-
                 <div className='photo'><img src={product.photo}/></div>
-
             <div className='actions'>
-
                 <button className='sell' disabled={!shoppingItem} onClick={removeShopping}>Sell</button>
-
                 <span className='amount'>{shoppingItem && shoppingItem.amount || 0}</span>
-
                 <button className='buy' disabled={total + product.price > money} onClick={addShopping}>Buy</button>
-
             </div>  
         </div>   
     </div>
-)
-}
+)}
