e0b438df207aafc65ed270314fb08912198fbc72
Zulkuf Demırhan <z.demirhan95@gmail.com>
2022-04-26 15:50:34 +0300

    Layout Fixed

--- Money-Spending-App/App.js
+++ Money-Spending-App/App.js
@@ -6,40 +6,27 @@ import products from './products.json'
 import { useState,useEffect } from 'react'
 import './Style.css'
 
-
-
-
 export default function App() {
 
     const [money,setMoney]=useState(100000);
-
     const [shopping , setShopping]=useState([]);
-
     const [total,setTotal]=useState();
-
     const reset = () => {
       setShopping([])
     }
-
     useEffect( () => {
-      
         setTotal(
           shopping.reduce((acc, item) => {
             return acc + (item.amount * (products.find(product => product.id === item.id).price))
-          },0),
-        )
-      
+          },0),)
     },[shopping])
   
   return (
     <div className='box'>
-
         <Main  money={money} total={total}/>
-        
         {products.map(product=>(
           <Product key={product.id} total={total} money={money} shopping={shopping} setShopping={setShopping} product={product}/>))}
         <Shopping reset={reset} total={total} products={products} shopping={shopping}/>
-
     </div>
   )
 }
